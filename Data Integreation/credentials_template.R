815f8136ffa49ce4a40c4eafb761a02b0f8d1202
Dino Karagic <66651925+Dinokgc@users.noreply.github.com>
2025-12-13 13:40:22 +0100

    Create credentials_template.R

---
+++ Data Integreation/credentials_template.R
@@ -0,0 +1,16 @@
+cred_psql_docker <- list(dbname = "xxxxxxxxxx",
+                         host   = "xxxxxxxx",
+                         user      = 'xxxxxxxxxxx',
+                         pass      = 'xxxxxxxxxxxxxxx',
+                         port = 5432)
+
+cred_psql_150 <- list(dbname = "xxxxxxxxxxxxxxx",
+                         host   = "xxxxxxxxxxxxxxxx",
+                         user      = 'xxxxxxxxxxxxxx',
+                         pass      = 'xxxxxxxxxxxxx',
+                         port = 6432)
+cred_psql_150_dvd <- list(dbname = "xxxxxxxxxxx",
+                          host   = "xxxxxxxxx",
+                          user      = 'xxxxx',
+                          pass      = 'xxxxxxxxxxx',
+                          port = 6432)
